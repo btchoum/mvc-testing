032912c5eb1a1afce4000957576d8bc8570d97a7
btchoum <bertrand.tchoumkeu@gmail.com>
2019-09-29 12:59:03 -0500

    This kinda works with Web API too

---
+++ RequestTracker/RequestTracker.Tests.Integration/Controllers/PersonControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using RequestTracker.Web.Controllers;
+using RequestTracker.Web.Models;
+using System;
+using System.Web;
+using System.Web.Http.Results;
+
+namespace RequestTracker.Tests.Integration.Controllers
+{
+    [TestFixture]
+    public class PersonControllerTests
+    {
+        [Test]
+        public void PersonController_Get()
+        {
+            var controller = CreateController();
+
+            var result = controller.Get() as OkNegotiatedContentResult<ApplicationUser>;
+            Assert.Multiple(() =>
+            {
+                Assert.That(result, Is.Not.Null);
+                Assert.That(result.Content, Is.Not.Null);
+                Assert.That(result.Content.Username, Does.Contain(Environment.UserName).IgnoreCase);
+            });
+        }
+
+        private static PersonController CreateController()
+        {
+            HttpContext.Current = MockHelpers.FakeHttpContext();
+
+            var controller = new PersonController();
+
+            return controller;
+        }
+    }
+}
---
+++ RequestTracker/RequestTracker.Web/Controllers/PersonController.cs
@@ -0,0 +1,25 @@
+﻿using RequestTracker.Web.Infrastructure;
+using RequestTracker.Web.Models;
+using System.Web.Http;
+
+namespace RequestTracker.Web.Controllers
+{
+    public class PersonController : ApiController
+    {
+        private readonly UserService _userService;
+
+        public PersonController()
+        {
+            _userService = new UserService();
+        }
+
+        [HttpGet]
+        [Route("api/people")]
+        public IHttpActionResult Get()
+        {
+            var person = _userService.GetLoggedInUser();
+
+            return Ok(person);
+        }
+    }
+}
